7bd7c7451f084cee9f2c478b7851b009cf4b1cd7
babludangi6266 <babludangi2000@gmail.com>
2026-08-12 16:14:37 +0530

    Add @CrossOrigin annotations to all controllers to prevent CORS 403 errors

--- backend/src/main/java/com/matchmind/controller/CandidateController.java
+++ backend/src/main/java/com/matchmind/controller/CandidateController.java
@@ -18,6 +18,7 @@ import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/candidates")
 @RequiredArgsConstructor
